cad73e8deb650a8a1f89773e256d5406197f5ac8
dinglq66 <dinglq66@gmail.com>
2021-07-27 14:02:07 +0800

    t265 codes updating

--- Realexp/test.py
+++ Realexp/test.py
@@ -6,15 +6,22 @@ import os
 
 def parser_args():
     parser = argparse.ArgumentParser(description='test the accel data in t265')
-    parser.add_argument('--save_dir', default='./data/t265_accel', help='dir to save the data')
+    parser.add_argument('--save_dir_accel', default='./data/t265_accel.txt', help='dir to save the accel data')
+    parser.add_argument('--save_dir_velocity', default='./data/t265_velocity.txt', help='dir to save the velocity data')
+    parser.add_argument('--save_dir_pose', default='./data/t265_pose.txt', help='dir to save the pose data')
     args = parser.parse_args()
     return args
 
+def write_data(filename, data):
+    fout = open(filename, 'a')
+    fout.write('{:.2f}\tx\t{:.4f}\ty\t{:.4f}\tz\t{:.4f}\n'.format(time.time(), data.x, data.y, data.z))
+    fout.close()
+    
 
 if __name__ == "__main__":
     args = parser_args()
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
+    # if not os.path.exists(args.save_dir):
+    #     os.makedirs(args.save_dir)
     
     # init the pipeline
     pipeline = rs.pipeline()
@@ -31,15 +38,17 @@ if __name__ == "__main__":
                 pos = data.translation
                 accel = data.acceleration
                 velocity = data.velocity
-                fout = open('./pose.txt', 'a')
-                accel_fout = open('./accel.txt', 'a')
-                velocity_fout = open('./velocity.txt', 'a')
-                fout.write('{:.2f}\tx\t{:.4f}\ty\t{:.4f}\tz\t{:.4f}\n'.format(time.time(), pos.x, pos.y, pos.z))
+                pose_fout = open(args.save_dir_pose, 'a')
+                accel_fout = open(args.save_dir_accel, 'a')
+                velocity_fout = open(args.save_dir_velocity, 'a')
+                pose_fout.write('{:.2f}\tx\t{:.4f}\ty\t{:.4f}\tz\t{:.4f}\n'.format(time.time(), pos.x, pos.y, pos.z))
                 accel_fout.write('{:.2f}\tx\t{:.4f}\ty\t{:.4f}\tz\t{:.4f}\n'.format(time.time(), accel.x, accel.y, accel.z))
                 velocity_fout.write('{:.2f}\tx\t{:.4f}\ty\t{:.4f}\tz\t{:.4f}\n'.format(time.time(), velocity.x, velocity.y, velocity.z))
                 print('the frame number is {}'.format(pose.frame_number))
                 print('the translation is x:{:.4f}, y:{:.4f}, z:{:.4f}'.format(pos.x, pos.y, pos.z))
-                fout.close()
+                pose_fout.close()
+                accel_fout.close()
+                velocity_fout.close()
     finally:
         pipeline.stop()
 
